872c655d885efd713726d3cd9d9455cf7df04a55
Juniz <fr.darktrooper08@gmail.com>
2025-04-07 17:19:20 +0200

    feat(cors): add CORS middleware to server and update dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes.js';
 import database from './database.js';
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use('/', router);
 
 app.use(cookieParser());
+app.use(cors());
 
 app.listen(SERVER_PORT, SERVER_HOST, () => {
   console.log(`Server is running on http://${SERVER_HOST}:${SERVER_PORT}`);
