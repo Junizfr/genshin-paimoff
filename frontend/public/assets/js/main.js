af56b0dd0cfd3d832320b0d2ff0635705af83b81
Juniz <fr.darktrooper08@gmail.com>
2025-04-07 21:46:05 +0200

    feat(alert): add alert component for success and error messages

--- frontend/public/assets/js/main.js
+++ frontend/public/assets/js/main.js
@@ -1,1 +1,13 @@
-import './components/navbar.js';
+//import './components/navbar.js';
+
+import { showAlert } from './components/alert.js';
+
+const showErrors = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const error = urlParams.get('error');
+  if (error) {
+    showAlert('error', error);
+  }
+};
+
+showErrors();
